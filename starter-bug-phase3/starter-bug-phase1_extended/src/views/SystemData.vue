d20a696746b8ff3b517243e05b319cfff5035fdc
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2021-04-12 19:52:16 +0200

    add base code for phase 3

---
+++ starter-bug-phase3/starter-bug-phase1_extended/src/views/SystemData.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>System Data</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
